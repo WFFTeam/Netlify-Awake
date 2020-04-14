476a800e7be407b9d2ef47fd0e0b8c563827e6a0
chaoabordo212 <chaoabordo@gmail.com>
2020-04-14 20:19:01 +0200

    Update ContactForm.vue

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,24 +1,10 @@
 <template>
-  <form name="contact" method="POST" data-netlify="true">
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>   
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: <select name="role[]" multiple>
-        <option value="leader">Leader</option>
-        <option value="follower">Follower</option>
-    </select></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
+  <form
+    name="Contact"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+    >
     <!-- Name -->
     <div class="field">
       <label class="label is-medium">Name</label>
