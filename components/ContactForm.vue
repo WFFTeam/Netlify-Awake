cd9d426dcae092cdc59318004394044ccf38545a
chaoabordo212 <chaoabordo@gmail.com>
2020-04-15 02:08:49 +0200

    Update ContactForm.vue

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,7 +1,5 @@
 <template>
-  <form name="Contact" netlify netlify-honeypot="bot-field" hidden>
-    <input type="text" name="Name" />
-    <input type="text" name="Email" />
+  <form>
     <!-- Name -->
     <div class="field">
       <label class="label is-medium">Name</label>
