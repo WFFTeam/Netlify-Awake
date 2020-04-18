5420d2082ac3c4532c8d47be2b90d3bf39ad46ec
chaoabordo212 <chaoabordo@gmail.com>
2020-04-18 18:41:32 +0200

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -19,8 +19,8 @@ export default {
   },
   mainMenu: [
     {
-      name: 'Experience',
-      link: '/experience'
+      name: 'Program',
+      link: '/program'
     },
     {
       name: 'About',
