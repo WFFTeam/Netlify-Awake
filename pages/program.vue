2eb4b92770f03c47f01cd439c0a9a98727cb0531
chaoabordo212 <chaoabordo@gmail.com>
2020-04-18 19:25:59 +0200

    Update program.vue

--- pages/program.vue
+++ pages/program.vue
@@ -15,32 +15,40 @@
             </strong>
           </p>
           <p>
-			Tourist Association of commune of Diano Marina – Sanremo, together with „Liguria Travel“
-			– a member of the International Dance Council CID, Paris - UNICEF, and College of Tourism Belgrade
+			Tourist Association of commune of Diano Marina – Sanremo,
+			together with „Liguria Travel“ – a member of the International Dance Council 
+			CID, Paris - UNICEF, and College of Tourism Belgrade
 			are organizing World Folklore Festival Diano Marina – Sanremo – Cannes.
-			This festival is one of the best and the most massive festivals of it's kind held in Europe and the world. 
-			It is the best precisely because of the selected top ensembles from the participant countries.
-			Due to extremely high interest of the folklore ensembles from all over the world, we are forced
-			to organize the festival in both the spring and the autumn. This program applies to both festivals.
+			This festival is one of the best and the most massive festivals
+			of it's kind held in Europe and the world. 
+			It is the best precisely because of the selected top ensembles
+			from the participant countries.
+			Due to extremely high interest of the folklore ensembles 
+			from all over the world, we are forced
+			to organize the festival in both the spring and the autumn.
+			This program applies to both festivals.
           </p>
 
           <h3>Performances</h3>
           <p>
-            All ensembles have four performances, defile in center of Diano Marina, performance on the stage,
-			performance in the center of Sanremo and performance on the world-renowned Promenade de la Croisette in Cannes,
+            All ensembles have four performances, defile in center of Diano Marina,
+			performance on the stage, performance in the center of Sanremo 
+			and performance on the world-renowned Promenade de la Croisette in Cannes,
 			right next to the iconic red carpet of Cannes Movie Festival.
           </p>
           <h3>Program by days</h3>
           <p>
             Note:
             <strong>This program applies both for the spring and the autumn festival</strong>
-			
+		  </p>
+          <p>
 			  <strong>1st day -</strong> Arrival of groups to Diano Marina and San Bartolomeo al Mare between 12 and 14h,
 			  trip to the hotel, trip to the stage where the festival will be held, rehearsal which is not mandatory,
 			  free time for sightseeing, dinner and trip back to the hotel for the night.
 			  Organizational board meeting will be held together with the representatives from all the groups at 21h,
 			  with short press conference, numerous journalists and TV reporters.
-			  
+		  </p>
+		  <p>
 			  <strong>2nd day –</strong> After the breakfast at 9 o'clock, facultative trip and sightseeing of enchanting
 			  French cities of Nice, Èze, Monaco and Monte Carlo. In afternoon hours there is a tour of a small, medieval town of Èze
 			  and the tour of  perfume factory Fragonard. After the tour we continue on the road to Nice, 
@@ -49,17 +57,21 @@
 			  We continue the trip onward to Monaco and Monte Carlo. The principality of Monacco and Monte Carlo is located
 			  between the Alps and the Mediterranean. These are mythical places which offer both dreams and reality.
 			  In the evening hours return to hotel.
-			  
+		  </p>
+		  <p>
 			  <strong>3rd day –</strong> Breakfast from 7:30 to 9h, after which participants change into costumes.
 			  At 10:30 is the departure to Sanremo and from 11:30 to 13h performance will be held with all the groups
 			  in the centre of pedestrian zone of Sanremo between world-famous theatre Ariston and Casino Sanremo. 
 			  At 13:30 we depart for Cannes. From 14:30 to 16:30 a performance will be held there with all of the groups
 			  on the world-renowned Croisette in front of a large audience. After that we return to the hotel, have a dinner and go to sleep	.
-
+		  </p>
+		  <p>
 			  <strong>4th day –</strong> After the breakfast there will be enough time for the preparation of all the gtoups
 			  for the defile and the main performance on the stage in Diano Marina. Defile starts at 15h in the center of Diano Marina,
 			  after which, at 16h, a performance will be held on beautiful stage which is right next to the sea.
 			  After the performances of all the ensembles there will be an award ceremony and party for all the participants of the festival.
+		  </p>
+		  <p>
 
               <strong>5th day –</strong> Breakfast. Hotel departure. Individual departures of the groups.
           </p>
@@ -68,37 +80,36 @@
             <strong>THE PRICE OF THE WHOLE PACKAGE IS 165 € per participant</strong>
 			
 			Package includes:
-			•	Lodgings in Diano Marina and San Bartolomeo al Mare 
+			*	Lodgings in Diano Marina and San Bartolomeo al Mare 
 				(beautiful connected small towns) in 3* hotels in two-bed, three-bed and four-bed rooms with breakfast
 				(Swedish table) and dinner
-			•	Services of permanent group guide
-			•	Festival participation
-			•	Organizer services
+			*	Services of permanent group guide
+			*	Festival participation
+			*	Organizer services
 			
 			Package <strong>DOES NOT</strong> include:
-			•	Transport of the groups
-			•	Parking for the buses (if and where it is needed).
-			•	Check point for entry into Monacco and Monte Carlo
-			•	Expenses of the international travel health insurance
-			•	Individual passenger's expenses
-			•	Extra payment for one-bed room
-			•	Expenses linked to the use of local buses for participants of the festival which will 	arrive by plane
+			*	Transport of the groups
+			*	Parking for the buses (if and where it is needed).
+			*	Check point for entry into Monacco and Monte Carlo
+			*	Expenses of the international travel health insurance
+			*	Individual passenger's expenses
+			*	Extra payment for one-bed room
+			*	Expenses linked to the use of local buses for participants of the festival which will 	arrive by plane
           </p>
 		  <h3>Propositions of the festival</h3>
           <p>
-
-			•	All ensembles have to prepare three different programs in maximum duration of 12 minutes per program
-			•	It is necessary to bring a flag from the country you came from and to display it during the performance
+			*	All ensembles have to prepare three different programs in maximum duration of 12 minutes per program
+			*	It is necessary to bring a flag from the country you came from and to display it during the performance
 				on the visible spot designated by the organizers
-			•	It is also necessary to bring a board with the name of the group and country from where the group comes from
-			•	Participating groups have to send 5 or 6 photographs and a video clip of the group's performance
-			•	Ensembles which don't have live music are required to bring a loudspeaker player on batteries for the performances in Sanremo, 
+			*	It is also necessary to bring a board with the name of the group and country from where the group comes from
+			*	Participating groups have to send 5 or 6 photographs and a video clip of the group's performance
+			*	Ensembles which don't have live music are required to bring a loudspeaker player on batteries for the performances in Sanremo, 
 				Cannes and the defile in Diano Marina, so they can perform their choreographies on open air space
-			•	Organizer holds the right to change some of the Festival General Conditions of which the participants will be informed in period of 24h.
-			•	If you decide to participate in the festival, the organizer will send an official invitation letter
-			•	Upon receiving the invitation letter you are obliged to, within 15 days of confirmation to pay the expense of the organizers
+			*	Organizer holds the right to change some of the Festival General Conditions of which the participants will be informed in period of 24h.
+			*	If you decide to participate in the festival, the organizer will send an official invitation letter
+			*	Upon receiving the invitation letter you are obliged to, within 15 days of confirmation to pay the expense of the organizers
 				in the amount of 30% of the total sum
-			•	Remaining 70% of the sum the participants will pay upon arrival at the hotel
+			*	Remaining 70% of the sum the participants will pay upon arrival at the hotel
           </p>
           <quote-card />
         </div>
